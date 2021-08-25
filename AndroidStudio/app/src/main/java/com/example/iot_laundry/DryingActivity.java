77dad9dc1a7deca8c540125ec325d413c288ae5b
sonmansu <gjdmstnals@pusan.ac.kr>
2021-08-26 02:01:47 +0900

    Add Firebase Project

--- AndroidStudio/app/src/main/java/com/example/iot_laundry/DryingActivity.java
+++ AndroidStudio/app/src/main/java/com/example/iot_laundry/DryingActivity.java
@@ -37,6 +37,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+
 import org.json.JSONException;
 
 import java.io.IOException;
