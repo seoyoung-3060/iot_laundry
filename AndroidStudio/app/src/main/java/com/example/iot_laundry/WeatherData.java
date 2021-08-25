cac62614a314332545e04d8c0065424720b6d4a8
sonmansu <gjdmstnals@pusan.ac.kr>
2021-08-25 19:07:08 +0900

    Cleanup Code

--- AndroidStudio/app/src/main/java/com/example/iot_laundry/WeatherData.java
+++ AndroidStudio/app/src/main/java/com/example/iot_laundry/WeatherData.java
@@ -97,13 +97,13 @@ public class WeatherData {
                         if (category.equals("SKY")) {
                             weather = "현재 날씨는 ";
                             if (fcstValue.equals("1")) {
-                                weather += "맑은 상태로";
+                                weather += "맑음";
                             } else if (fcstValue.equals("2")) {
-                                weather += "비가 오는 상태로 ";
+                                weather += "비";
                             } else if (fcstValue.equals("3")) {
-                                weather += "구름이 많은 상태로 ";
+                                weather += "구름많음";
                             } else if (fcstValue.equals("4")) {
-                                weather += "흐린 상태로 ";
+                                weather += "흐림";
                             }
                         }
 
