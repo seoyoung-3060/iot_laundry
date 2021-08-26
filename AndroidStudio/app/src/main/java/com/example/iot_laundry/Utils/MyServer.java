3fe772ff64f4d9cc40045a6ab51b1de679b1efd1
sonmansu <gjdmstnals@pusan.ac.kr>
2021-08-26 23:00:57 +0900

    Fix Reload Button Error

--- AndroidStudio/app/src/main/java/com/example/iot_laundry/Utils/MyServer.java
+++ AndroidStudio/app/src/main/java/com/example/iot_laundry/Utils/MyServer.java
@@ -4,8 +4,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class MyServer {
-//    public static String serverAddress;// = "192.168.35.237";
-    public static String serverAddress = "172.30.1.44";
+//    public static String serverAddress;// = "192.168.35.237";   //수민
+    public static String serverAddress = "192.168.35.236";   //수민
+//    public static String serverAddress = "172.30.1.44"; //커피나무
 
 //    public void setServerAddress(String serverAddress) {
 //        this.serverAddress = serverAddress;
